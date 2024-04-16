9646d18b69a1784d22ce35c040a343335e5eefdf
ca8maristela <57410056+ca8maristela@users.noreply.github.com>
2024-04-16 16:51:42 -0700

    Added basic google Ai file

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+
+import google.generativeai as genai
+
+# Using `GOOGLE_API_KEY` environment variable.
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=GOOGLE_API_KEY)
+
+"""# Checking for all available models with generateContent feature
+for model in genai.list_models():
+    if "generateContent" in model.supported_generation_methods:
+        print(model.name) """
+
+# Initiate the Model
+model = genai.GenerativeModel(model_name="gemini-pro")
+
+# Query the Model
+response = model.generate_content(
+    "Answer this question in a JSON format: What video games should I play?")
+
+print(response.text)
