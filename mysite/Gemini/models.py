0ef6c684e2e30b6f7726c51fde3a13a87af65be9
ca8maristela <57410056+ca8maristela@users.noreply.github.com>
2024-07-21 00:22:42 -0700

    Added new model for storing games generated

--- mysite/Gemini/models.py
+++ mysite/Gemini/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Game(models.Model):
+    """gameTitle = models.CharField(max_length=100)
+    setting = models.TextField()
+    story = models.TextField()
+    lore = models.TextField()
+    characters = models.TextField()
+    enemies = models.TextField()
+    levels = models.TextField()
+    bosses = models.TextField()
+    equipment = models.TextField()
+    gameplay = models.TextField()
+    features = models.TextField() """
+    game = models.TextField()
