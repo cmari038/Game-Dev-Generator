dd88718de3fb9054ab617ef98b155b263d22b645
ca8maristela <57410056+ca8maristela@users.noreply.github.com>
2024-08-20 12:02:22 -0700

    Revert "Removed sign in files"
    This reverts commit 84f3dad6360e04412f3eb4d8127a0b699668d84a.

---
+++ my-app/src/LogIn.js
@@ -0,0 +1,131 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "./Firebase";
+
+const LogIn = () => {
+
+const [email, getEmail] = useState('');
+const [password, getPassword] = useState('');
+
+const setEmail = (email) => {
+    getEmail(email);
+};
+
+const setPassword = (pwd) => {
+    getPassword(pwd);
+};
+
+const [error, getError] = useState('');
+
+const setError = (issue) => {
+    getError(issue);
+}
+
+const navigate = useNavigate();
+
+const signIn = async (e) => {
+    e.preventDefault();
+    
+    await signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user);
+        navigate("/home");
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        setError(errorMessage);
+    });
+}
+
+/*
+useEffect(() => {
+
+const signIn = async (e) => {
+    e.preventDefault();
+
+    await signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
+}
+
+if (click) {
+    signIn(auth, email, password);
+    navigate("/home");
+}
+
+}, [click, email, password, navigate]); */
+
+
+return (
+    <form>
+        <div style={{textAlign: "center"}}>
+
+            <h1>Log In</h1>
+
+            <label>
+           <h2>Email</h2>
+                <input
+                    type="text"
+                    value={email}
+                    onChange={e=>setEmail(e.target.value)}
+                >
+                </input>
+
+            </label>
+            
+
+            <label>
+            <h3>Password</h3>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={e=>setPassword(e.target.value)}
+                >
+                </input>
+
+            </label>
+
+            <button type="submit" onClick={signIn} style={{textAlign: "center"}}>
+                            Log in
+            </button>
+
+        <Link to="/makeAccount">
+        <button>
+            Sign Up
+        </button>
+        </Link>
+
+
+        <label>
+            {error}
+        </label>
+
+
+        </div>
+        </form>
+
+    ); 
+   
+};
+
+export default LogIn;
+//export const Email = email;
+//export const email;
+
+//<Link to= "/">
+//<button>Games To Make</button>
+//</Link>
---
+++ my-app/src/MakeAccount.js
@@ -0,0 +1,112 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "./Firebase";
+
+
+const MakeAccount = () => {
+
+const [email,getEmail] = useState('');
+const [password, getPassword] = useState('');
+
+const setEmail = (email) => {
+   getEmail(email);
+}
+
+const setPassword = (pwd) => {
+    getPassword(pwd);
+}
+
+const [error, getError] = useState('');
+
+const setError = (issue) => {
+    getError(issue);
+}
+
+const navigate = useNavigate();
+
+const createAccount = async(e) => {
+    e.preventDefault();
+
+    await createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        console.log(user);
+        navigate("/")
+
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        setError(errorMessage);
+        // ..
+    });
+}
+/*useEffect(() => {
+const createAccount = async(auth, email, password) => {
+    await createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+    });
+} 
+
+if(click) {
+    createAccount(auth,email,password);
+} 
+
+}, [click, email, password] ); */
+
+    return (
+        <form>
+        <div style={{textAlign: "center"}}>
+
+            <h1>Make an Account</h1>
+
+            <label>
+           <h2>Enter an Email</h2>
+                <input
+                    type="text"
+                    value={email}
+                    onChange={e=>setEmail(e.target.value)}
+                >
+                </input>
+
+            </label>
+
+            <label>
+            <h3>Enter a password</h3>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={e=>setPassword(e.target.value)}
+                >
+                </input>
+
+            </label>
+
+            <button type="submit" onClick={createAccount}>
+                Create Account
+            </button>
+            
+            <label>
+                {error}
+            </label>
+
+        </div>
+    </form>
+    );
+   
+};
+
+export default MakeAccount;
